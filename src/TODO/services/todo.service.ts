f1e9f192c8d59410a00cf32cf9d0fd80dabdd8a3
João Victor Carvalho <carvalhojoaovi20@gmail.com>
2022-10-20 15:57:24 -0300

    New Update:
    The 'Create' was added in ToDo service

--- src/TODO/services/todo.service.ts
+++ src/TODO/services/todo.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
-import { TodoList } from "../entities/todo.entities";
+import { TodoList, TodoList } from "../entities/todo.entities";
 
 @Injectable()
 export class TodoListService {
@@ -17,7 +17,16 @@ export class TodoListService {
 
     async findById(id: number): Promise<TodoList>{
         let list = await this.todolistRepository.findOne({
-            where: id
-        })
+        where: {
+            id
+        }
+        });
+        if(!TodoList)
+        throw new HttpException('To Do List not found', HttpStatus.NOT_FOUND);
+        return list
+    }
+
+    async create(TodoList: TodoList) : Promise<TodoList>{
+        return await this.todolistRepository.save(TodoList);
     }
 }
